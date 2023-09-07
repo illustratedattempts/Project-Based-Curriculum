cd2e028c4be4829a0393d73c332b8519cb24fff2
Thomas Vo <94024184+illustratedattempts@users.noreply.github.com>
2023-09-07 13:04:02 -0500

    Add files via upload

---
+++ Proj2_Practice.py
@@ -0,0 +1,50 @@
+# Fill in the missing pieces of code to make the program work!
+# NOTE TO SELF DO NOT MOVE THE LINES! Just remove the """s
+# PART 1
+Bot_Name =   # <---- Give your bot a name
+Your_Name =   # <---- Insert your name here
+print(Bot_Name + ": " + "Hello! " + Your_Name + "!")
+
+"""
+# Part 2 START -- Delete the above line when Part 1 is complete
+# Bot asking about the weather
+Weather_Condition =  # <---- Type the weather condition here
+print(Bot_Name + ": " + "Have you seen the weather lately? I think it's " + Weather_Condition + " outside.")
+"""
+# Part 2 END -- Delete the above line when Part 1 is complete
+
+"""
+# Part 3 START -- Delete the above line when Part 2 is complete
+# Bot asking for a topic that you have
+Bot_Question1 = "What's do you want to talk about today?"
+print(Bot_Name + ": " + ) # <---- How do I make the bot ask the question? #What do I need to add? There's only ONE thing missing!
+Your_Response1 = # <---- Enter your response to the question
+print(Your_Name + ": " + ) # <---- How do I make it so that it says your #response? There's only ONE thing missing!
+"""
+# Part 3 END -- Delete the above line when Part 2 is complete
+
+"""
+# Part 4 START -- Delete the above line when Part 3 is complete
+# Bot replying to your topic
+Bot_Response1 = "I understand. You want to talk about " + # <---- How do I #make the bot say what you wanted to talk about...
+print() # <---- Add code to make the bot say its response. Kind of like #above (if done correctly)
+"""
+# Part 4 END -- Delete the above line when Part 3 is complete
+
+"""
+# Part 5 START -- Delete the above line when Part 4 is complete
+# Bot asks what you like about your topic
+Your_Topic = Your_Response1 # <---- I am copying the value of Your_Response1 #into a new variable
+Bot_Question2 = "What do you like about " + + "?" # <---- What do you add in #the middle of the plus signs?
+print() # <---- Add missing code to print out the response like the code #above
+
+"""
+# Part 5 END -- Delete the above line when Part 4 is complete
+
+"""
+# Part 6 START -- Delete the above line when Part 5 is complete
+# Your response to the question
+Your_Response2 = # <---- Enter your response to the question (a sentence)
+print() # <---- Add missing code to print out your response like the code #above
+"""
+# Part 6 START -- Delete the above line when Part 5 is complete
